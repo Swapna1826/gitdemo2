c11cb35c13acdf660a68538efe7ee6b63eb8264c
1111S <1111S@LAPTOP-VDTIOAN4>
2022-01-06 19:36:34 +0530

    First commit.....

---
+++ src/gittutorial/FirstDemo.java
@@ -0,0 +1,17 @@
+package gittutorial;
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		 String hungry =" yes";
+		 if(hungry ==" yes") {
+			 System.out.println("eat some thing");
+		 }
+		 else {
+			 System.out.println("do some work"); 
+		 }
+		}
+
+	
+
+}
